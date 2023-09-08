90b1cdb8aaceded0b848c56dd94a068a588946fd
nisar2110 <nisarahamed.mvit@gmail.com>
2023-09-08 18:15:45 +0530

    product view file added

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
-import React, { useEffect, useState } from "react";
-import { createBrowserRouter, RouterProvider, Outlet, Link } from "react-router-dom";
+import React, { lazy, useEffect, useState } from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Link,
+} from "react-router-dom";
 import ReactDOM from "react-dom/client";
 
-import Header from "./components/Header";
+const Header = lazy(() => import("./components/Header"));
+const ProductView = lazy(() => import("./components/ProductView"));
 
-import { HeartIcon, ShoppingBagIcon, ShoppingCartIcon, FaceFrownIcon, FaceSmileIcon, BellIcon } from '@heroicons/react/24/outline'
-
+import {
+  HeartIcon,
+  ShoppingBagIcon,
+  ShoppingCartIcon,
+  FaceFrownIcon,
+  FaceSmileIcon,
+  BellIcon,
+} from "@heroicons/react/24/outline";
 
 const AppLayout = () => {
-    return (
-        <>
-            <Header />
-        </>
-    )
-}
-
-
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+};
 
 const appRouter = createBrowserRouter([
-    {
-        path: "/",
-        element: <AppLayout />,
-        children: [
-
-        ],
-        errorElement: <Error />,
-    },
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/product/men",
+        element: <ProductView />,
+      },
+      {
+        path: "/product/women",
+        element: <ProductView />,
+      },
+    ],
+    errorElement: <Error />,
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<RouterProvider router={appRouter} />);
-
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,56 +1,55 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import { HeartIcon, ShoppingBagIcon, UserIcon } from '@heroicons/react/24/outline'
+import {
+  HeartIcon,
+  ShoppingBagIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
 
-const navigation = [
-    { name: 'Home', href: '/home', current: true },
-    { name: 'Women', href: '/product/women', current: false },
-    { name: 'Man', href: '/product/man', current: false },
-]
+const leftHeaderMenu = [
+  { name: "Women", href: "/product/women", current: false },
+  { name: "Men", href: "/product/men", current: false },
+];
+
+const rightHeaderMenu = [
+  { name: "Profile", href: "/profile", current: false },
+  { name: "Wishlist", href: "/wishlist", current: false },
+  { name: "Bag", href: "/checkout/cart", current: false },
+];
 
 const Header = () => {
-    return (
-        <div className="space-x-4 w-full flex h-16 items-center justify-between fixed top-0 shadow-lg">
-            <div className="flex items-center ml-8">
-                <div className="px-5">
-                    <Link to="/home">
-                        <img
-                            className="h-8  w-auto"
-                            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                            alt="Your Company"
-                        />
-                    </Link>
-                </div>
-
-                {navigation.map((item) => (
-                    <div className=" block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                        <Link to={item.href}>{item.name}</Link>
-                    </div>
-                ))}
-            </div>
-            <div className="flex items-center !mr-16">
-                <div className="font-bold text-xs px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
-                    <Link className="flex flex-col items-center" to="/notification">
-                        <UserIcon className="h-6 w-6" aria-hidden="true" />
-                        <div className="text-center">Profile</div>
-                    </Link>
-                </div>
-                <div className="text-xs font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
-                    <Link className="flex flex-col items-center" to="/wishlist">
-                        <HeartIcon className="h-6 w-6" aria-hidden="true" />
-                        <div className="text-center">Wishlist</div>
-                    </Link>
-                </div>
-                <div className="text-xs font-bold px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
-                    <Link className="flex flex-col items-center" to="/checkout/cart">
-                        <ShoppingBagIcon className="h-6 w-6" aria-hidden="true" />
-                        <div className="text-center">Wishlist</div>
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div className="space-x-4 w-full flex h-16 items-center justify-between fixed top-0 shadow-lg">
+      <div className="flex items-center ml-8">
+        <div className="px-5">
+          <Link to="/home">
+            <img
+              className="h-8  w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+              alt="Your Company"
+            />
+          </Link>
         </div>
-    )
-}
+
+        {leftHeaderMenu.map((lh) => (
+          <div className=" block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+            <Link to={lh.href}>{lh.name}</Link>
+          </div>
+        ))}
+      </div>
+      <div className="flex items-center !mr-16">
+        {rightHeaderMenu.map((rh) => (
+          <div className="font-bold text-xs px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">
+            <Link className="flex flex-col items-center" to={rh.href}>
+              <UserIcon className="h-6 w-6" aria-hidden="true" />
+              <div className="text-center">{rh.name}</div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Header;
